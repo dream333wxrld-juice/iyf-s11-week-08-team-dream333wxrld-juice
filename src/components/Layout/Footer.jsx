36d099e839e9a15c74297339959cb43861a8b1ed
kaywambui-1 <kamauchristine85@gmail.com>
2026-08-06 05:04:48 +0300

    update src/components/Layout/Footer.jsx src/components/User/UserCard.jsx src/components/User/UserProfile.jsx

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <nav className="footer">
+                <a href="#">About</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Contact</a>
+            </nav>
+            <p>&copy; 2026 CommunityHub. All rights reserved.</p>
+
+        </footer>
+    );
+}
+
+export default Footer;
